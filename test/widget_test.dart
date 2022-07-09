d9b08f87b779491d7bff0527fd28cc469285995c
blast06 <lacalculadora06@hotmail.com>
2022-07-09 12:02:54 -0400

    solved imports at widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:generate_pdf_invoice_example/main.dart';
 
-import 'package:invoice/main.dart';
+
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
